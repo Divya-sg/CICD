4c137ae71463f575c1e351431cdae7ff9994eb28
Vidyaraman <krsna@Vidyaramans-iMac.local>
2022-05-14 10:37:06 +0800

    update1

--- imgwebapp.py
+++ imgwebapp.py
@@ -9,7 +9,7 @@ import requests
 from io import BytesIO
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
-st.title("Bean Image Classifier")
+st.title("Leaf Image Classifier")
 st.text("Provide URL of bean Image for image classification")
 
 @st.cache(allow_output_mutation=True)
@@ -23,7 +23,7 @@ with st.spinner('Loading Model Into Memory....'):
 classes=['angular_leaf_spot','bean_rust','healthy']
 
 def decode_img(image):
-  img = tf.image.decode_jpeg(image, channels=3)  
+  img = tf.image.decode_jpeg(image, channels=3)
   img = tf.image.resize(img,[224,224])
   return np.expand_dims(img, axis=0)
 
@@ -34,7 +34,7 @@ if path is not None:
     st.write("Predicted Class :")
     with st.spinner('classifying.....'):
       label =np.argmax(model.predict(decode_img(content)),axis=1)
-      st.write(classes[label[0]])    
+      st.write(classes[label[0]])
     st.write("")
     image = Image.open(BytesIO(content))
     st.image(image, caption='Classifying Bean Image', use_column_width=True)
